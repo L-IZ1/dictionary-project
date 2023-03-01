d5af83a9181781e7e55e3d7b7fdfd515b5f3a511
L-IZ1 <111218789+L-IZ1@users.noreply.github.com>
2023-03-01 18:02:05 +0100

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
         Hello
       </header>
-    </div>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="text-center">Coded by SheCodes</footer>
+    </div></div>
   );
 }
 
